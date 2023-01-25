cde2007dd59b6c7c0030c9cc2d09da0a0682cfdc
Silpi Sulekha Sahoo <Silpi.Sahoo@ATMECS.CORP>
2023-01-25 21:59:33 +0530

    mockAPI

---
+++ PlaywrightJavatask/PlaywrightMOCKAPI/MockAPITest2.java
@@ -0,0 +1,49 @@
+package API;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockAPITest2 {
+    Playwright playwright;
+    APIRequestContext apiRequestContext;
+    Map<String, String> headers;
+    @BeforeClass
+    public  void setup(){
+        playwright = Playwright.create();
+        headers = new HashMap<>();
+    }
+
+    @Test
+    public void getCallTest() {
+        headers.clear();
+        //headers.put("Content-Type", "application/json");
+        apiRequestContext = playwright.request().newContext(new APIRequest.NewContextOptions()
+                .setBaseURL("https://2aa08edc-409f-46ab-b3bf-1260b5622d88.mock.pstmn.io")
+                .setExtraHTTPHeaders(headers));
+        APIResponse resp = apiRequestContext.get("/getUsers");
+        String response= resp.text();
+        System.out.println("------"+response);
+
+        JsonObject json = new Gson().fromJson(response,JsonObject.class);
+        System.out.println(json.get("name"));
+
+    }
+
+    @AfterClass
+    public  void  tearDown(){
+        apiRequestContext.dispose();
+       playwright.close();
+
+    }
+
+}
